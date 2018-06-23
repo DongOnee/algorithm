ec8bd5e088867dbc4204ad78dfc6736a2ad2e85c
DongOnee <dongwon5520@gmail.com>
2018-06-23 13:28:16 +0900

    scpc

--- baekjoon/10172.c
+++ baekjoon/10172.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 
-int main(void) {
-  printf("|\\_/|\n|q p|   /}\n( 0 )\"\"\"\\\n|\"^\"`   |\n||_/=\\__|");
+int main (void) {
+  printf("|\\_/|\n");
+  printf("|q p|   /}\n");
+  printf("( 0 )\"\"\"\\\n");
+  printf("|\"^\"`    |\n");
+  printf("||_/=\\\\__|\n");
+
+  return 0;
 }
