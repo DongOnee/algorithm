e6934235767fab1fb28c107eb735d3925cd90484
DongOnee <dongwon5520@gmail.com>
2018-06-24 04:10:11 +0900

    linux

--- scpc_2018/1.c
+++ scpc_2018/1.c
@@ -1,5 +1,5 @@
 /*
-You should use the statndard input/output
+You should use the standard input/output
 
 in order to receive a score properly.
 
@@ -12,23 +12,68 @@ Please be very careful.
 
 int Answer;
 
-typedef struct Bus {
-  int* person[200000];
-  int count;
-} B;
+typedef struct node {
+  int a;
+  struct node* next;
+} node;
 
-void in_bus(B* bus, int* person) {
-  bus->count++;
-  bus->person[bus->count] = person;
+typedef struct bus {
+  node *start;
+  node *end;
+  int size;
+} bus;
+
+int n, k;
+
+node a[200000];
+bus b[200000];
+
+int check(int bus_index, int input_index) {
+  node *tmp = b[bus_index].start;
+  while ( tmp!=NULL ) {
+		int diff = tmp->a - a->a;
+		if (diff < 0) diff *=-1;
+		if (diff <= k) return 0;
+  }
+	return 1;
 }
 
-void out_bus(B* bus) {
-  bus->count--;
+void bus_input(int bus_index, int input_index) {
+	if (b[bus_index].size == 0) b[bus_index].start =a;
+	else b[bus_index].end->next = a;
+	b->end = a;
+	b->size++;
 }
 
-int is_empty(B* bus) {
-  if (bus->count ==0) return 1;
-  return 0;
+void bus_output(bus *b) {
+	node *tmp = b->start;
+	if (b->size == 1) {
+		b->start = NULL;
+		b->end = NULL;
+		b->size--;
+	} else {
+		for (int i=2; i<b->size; i++) tmp = tmp->next;
+		b->end = tmp;
+		tmp->next = NULL;
+		b->size--;
+	}
+}
+
+void dfs(int input_index, int bus_count) {
+  if (input_index == n) {
+    if (bus_count < Answer) Answer = bus_count;
+    return;
+  }
+  for(int i=0; i<bus_count; i++) {
+    if(check(i, input_index)) {
+      bus_input(i, input_index);
+      dfs(input_index+1, bus_count);
+      bus_output(i);
+    }
+  }
+  bus_input(bus_count+1, input_index);
+  dfs(input_index+1, bus_count+1);
+  bus_output(bus_count+1);
 }
 
 int main(void)
@@ -42,37 +87,30 @@ int main(void)
 	   You may remove the comment symbols(//) in the below statement and use it.
 	   But before submission, you must remove the freopen function or rewrite comment symbols(//).
 	 */
-	freopen("input.txt_1", "r", stdin);
+	freopen("sample_input_1.txt", "r", stdin);
 
 	/*
-	   If you remove the statement below, your program's output may not be rocorded
+	   If you remove the statement below, your program's output may not be recorded
 	   when your program is terminated after the time limit.
 	   For safety, please use setbuf(stdout, NULL); statement.
 	 */
-	setbuf(stdout, NULL);
+	//setbuf(stdout, NULL);
 
 	scanf("%d", &T);
 	for(test_case = 0; test_case < T; test_case++)
 	{
-    /////////////////////////////////////////////////////////////////////////////////////////////
-    /*
+
+		/////////////////////////////////////////////////////////////////////////////////////////////
+		/*
 		   Implement your algorithm here.
 		   The answer to the case will be stored in variable Answer.
 		 */
-     Answer =0;
-     int n, k;
-     scanf("%d %d", &n, &k);
-     int a[200000];
-     for (int i=0; i<n; i++) scanf("%d", &a[i]);
-
-     for (int i=0; i<n; i++) {
-       for (int j=0; j<n_bus; j++) {
-         in_bus(buses[j], a[i]);
-       }
-     }
-
-
 		/////////////////////////////////////////////////////////////////////////////////////////////
+    scanf("%d %d", &n, &k);
+    for (int i=0; i<n; i++) scanf("%d", &a[i].a);
+		
+
+    dfs(0, 0);
 
         		// Print the answer to standard output(screen).
 
