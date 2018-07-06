8ef5b1e2e8075ac2b2ebacf9cc2ed0d039aeb257
kangdongwon <dongwon5520@gmail.com>
2018-07-06 09:44:24 +0900

    18.7.6 mac

--- baekjoon/n_1019.c
+++ baekjoon/n_1019.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
 
-int main(void) {
-    unsigned int a;
-    unsigned int c[10]={};
-    scanf("%d",&a);
-    while(a--){
-        unsigned int j;
-        j =a+1;
-        while(j!=0){
-            c[j%10]++;
-            j=j/10;
-        }
-    }
-    printf("%d %d %d %d %d %d %d %d %d %d\n",c[0] ,c[1] ,c[2] ,c[3] ,c[4] ,c[5] ,c[6] ,c[7] ,c[8] ,c[9]);
+int main(int argc, char const *argv[])
+{
+    int n;
+    int d;
+
+    scanf("%d", &n);
+
+
     return 0;
 }
