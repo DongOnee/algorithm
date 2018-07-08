7bc175e354842afdb86d1ca83b2109a01ac13dd3
kangdongwon <dongwon5520@gmail.com>
2018-07-08 09:42:44 +0900

    18.7.8 mac

--- baekjoon/n_1068.c
+++ baekjoon/n_1068.c
@@ -1,53 +1,20 @@
 #include <stdio.h>
 
-void deleteOperation(int, int[], int);
-void countleafnode(int[], int);
+int d[51][51] ={ -1, };
+int sol;
 
-int main(void) {
-  // the # of nodes
-  int N;
-  scanf("%d",&N);
+int q[51];
+int top, bot;
 
-  // initialize nodes
-  int parentofi[N];
-  for (int i = 0; i < N; i++)
-    scanf("%d",&parentofi[i]);
-  int nodesize = sizeof(parentofi)/sizeof(int);
-
-  // get deletenode's index
-  int deletenode;
-  scanf("%d",&deletenode);
-  parentofi[deletenode]=-1;
-
-  // delete!!
-  deleteOperation(deletenode, parentofi, nodesize);
-
-  // delete node without leafnode
-  countleafnode(parentofi, nodesize);
-
-  // count leafnode
-  int solution=0;
-  for (int i = 0; i < N; i++) {
-    if(parentofi[i]!=-1) solution++;
-  }
-  printf("%d\n",solution);
-  return 0;
-}
-
-void deleteOperation(int index, int nodes[], int nodesize) {
-  nodes[index]=-1;
-  for (int i=0;i<nodesize;i++){
-    if(nodes[i]==index) deleteOperation(i, nodes, nodesize);
-  }
-}
-
-void countleafnode(int nodes[], int nodesize){
-  for (int i = 0; i < nodesize; i++) {
-    for (int j = 0; j < nodesize; j++) {
-      if (nodes[j]==i) {
-        nodes[i]=-1;
-        break;
-      }
+int main(int argc, char const *argv[])
+{
+    int n;
+    scanf("%d", &n);
+    for(int i=0; i<n; i++)
+    {
+        int tmp;
+        scanf("%d", &tmp);
     }
-  }
+    /* code */
+    return 0;
 }
