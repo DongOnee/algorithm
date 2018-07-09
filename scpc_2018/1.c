62b4f9a4802a62211120d9d8ddfe69ad7d947f88
DongOnee <dongwon5520@gmail.com>
2018-07-10 00:04:57 +0900

    18.7.10 linux

--- scpc_2018/1.c
+++ scpc_2018/1.c
@@ -12,6 +12,73 @@ Please be very careful.
 
 int Answer;
 
+typedef struct node {
+  int val;
+  struct node *next;
+} node;
+
+typedef struct bus {
+  node *start;
+  node *end;
+  int size;
+} bus;
+
+node A[200000];
+bus B[200000];
+
+int n, k;
+
+int in_bus(int input_index, int bus_index) {
+  if(B[bus_index].size == 0) {
+    B[bus_index].start = &A[input_index];
+    B[bus_index].end = &A[input_index];
+    B[bus_index].size++;
+    return 1;
+  }
+  node *tmp = B[bus_index].start;
+  while ( tmp != NULL ) {
+    int diff = tmp->val - A[input_index].val;
+    if (diff < 0) diff *=-1;
+    if (diff <= k) return 0;
+    tmp = tmp->next;
+  }
+  B[bus_index].end->next = &A[input_index];
+  B[bus_index].end = &A[input_index];
+  B[bus_index].size++;
+  return 1;
+}
+
+void out_bus(int bus_index) {
+  node *tmp = B[bus_index].start;
+  if(B[bus_index].size == 1) {
+    B[bus_index].start = NULL;
+    B[bus_index].end = NULL;
+    B[bus_index].size--;
+  } else {
+    for (int i=2; i<B[bus_index].size; i++) tmp = tmp->next;
+    tmp->next = NULL;
+    B[bus_index].end = tmp;
+    B[bus_index].size--;
+  }
+}
+
+void dfs(int input_index, int bus_count) {
+  if (input_index == n) {
+    if (bus_count < Answer) Answer = bus_count;
+    return;
+  }
+  for (int i=0; i<bus_count; i++) {
+    if(in_bus(input_index, i)){
+      dfs(input_index+1, bus_count);
+      out_bus(i);
+    }
+  }
+  in_bus(input_index, bus_count);
+  if (bus_count+1 < Answer)
+    dfs(input_index+1, bus_count+1);
+  out_bus(bus_count);
+}
+
 int main(void)
 {
 	int T, test_case;
@@ -23,14 +90,14 @@ int main(void)
 	   You may remove the comment symbols(//) in the below statement and use it.
 	   But before submission, you must remove the freopen function or rewrite comment symbols(//).
 	 */
-	// freopen("input.txt", "r", stdin);
+	// freopen("sample_input_1.txt", "r", stdin);
 
 	/*
 	   If you remove the statement below, your program's output may not be recorded
 	   when your program is terminated after the time limit.
 	   For safety, please use setbuf(stdout, NULL); statement.
 	 */
-	//setbuf(stdout, NULL);
+	setbuf(stdout, NULL);
 
 	scanf("%d", &T);
 	for(test_case = 0; test_case < T; test_case++)
@@ -44,6 +111,14 @@ int main(void)
 		/////////////////////////////////////////////////////////////////////////////////////////////
 
         		// Print the answer to standard output(screen).
+    scanf("%d %d", &n, &k);
+    for (int i=0; i<n; i++) {
+      scanf("%d", &A[i].val);
+      A[i].next = NULL;
+    }
+    Answer = n;
+
+    dfs(0,0);
 
 		printf("Case #%d\n", test_case+1);
 	        	printf("%d\n", Answer);
