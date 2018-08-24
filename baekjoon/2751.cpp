8d6ec47fcb0b8eeb61a924f09f0c2b1e08413ff5
DongOnee <dongwon5520@gmail.com>
2018-08-24 21:46:01 +0900

    18.8.24

--- baekjoon/2751.cpp
+++ baekjoon/2751.cpp
@@ -1,46 +1,16 @@
-/**
- * MAC MAC MAC
- * COMPUTER2 Modify2 ~~~~
- * I hate GIT!
- * TEST Brunch
- */
-
-
 #include <iostream>
-
 using namespace std;
 
 int d[1000000];
 
-void insert(int in, int pos, int end_pos)
+void sort(int input, int insert_pos)
 {
-    for (int i=end_pos; pos<=i; i--) d[i+1] = d[i];
-    d[pos] = in;
-}
+    int start=0;
+    int end = insert_pos-1;
+    int mid = (start + end)/2;
+    while(true)
+    {
 
-void sort(int in, int in_pos)
-{
-    if(in_pos == 0)
-    {
-        d[0] = in;
-        return;
-    } else if (in_pos == 1)
-    {
-        if (d[0] < in ) d[1] = in;
-        else {
-            d[1] = d[0];
-            d[0] = in;
-        }
-        return;
-    }
-    int start = 0;
-    int end = in_pos-1;
-    int mid = (start+end)/2;
-    while(start != mid)
-    {
-        if (in <= d[mid]) end = mid;
-        else start = mid;
-        mid = (start+end)/2;
     }
 }
 
