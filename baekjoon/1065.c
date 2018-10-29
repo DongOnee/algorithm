c074117919732064ba844867f76e0ffa305ae37a
kangdongwon <dongwon5520@gmail.com>
2018-10-29 15:54:31 +0900

    tmp? ?

--- baekjoon/1065.c
+++ baekjoon/1065.c
@@ -3,24 +3,24 @@
 int hannumber(int);
 
 int main(void) {
-  int N;
-  scanf("%d", &N);
+    int N;
+    scanf("%d", &N);
 
-  int count = 0;
-  for (int i = 1; i <= N; i++) {
-    if(hannumber(i)) count++;
-  }
-  printf("%d\n", count);
-  return 0;
+    int count = 0;
+    for (int i = 1; i <= N; i++) {
+        if(hannumber(i)) count++;
+    }
+    printf("%d\n", count);
+    return 0;
 }
 
 int hannumber(int i) {
-  if (i/100 == 0) return 1;
-  else{
-    while( (i % 10 - i / 10 % 10) == (i / 10 % 10 - i / 100 % 10) ) {
-      i = i/10;
-      if(i/100==0) return 1;
+    if (i/100 == 0) return 1;
+    else {
+        while( (i % 10 - i / 10 % 10) == (i / 10 % 10 - i / 100 % 10) ) {
+            i = i/10;
+            if(i/100==0) return 1;
+        }
+        return 0;
     }
-    return 0;
-  }
 }
