dc1104969f67c3fcf7c9c71ca59cbf7fd9ccac6f
Dongonee <dongwon5520@gmail.com>
2019-07-10 07:58:21 +0900

    tmp

---
+++ baekjoon/11657.cpp
@@ -0,0 +1,16 @@
+/**
+ * 2019.07.10. 07:57 ~
+ */
+#include <cstdio>
+
+int n, m;
+
+int main(int argc, char const *argv[])
+{
+    scanf("%d%d", &n, &m);
+    for (int i=0; i<m; i++)
+    {
+        
+    }
+    return 0;
+}
