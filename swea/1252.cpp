78582529073f576fb6cc54332b9eab343a1200b3
Dongonee <dongwon5520@gmail.com>
2019-07-12 00:08:05 +0900

    SWEA#1252 tmp

--- swea/1252.cpp
+++ swea/1252.cpp
@@ -2,37 +2,121 @@
  * 2019.07.11. 08:11 ~
  */
 #include <cstdio>
+#include <cstring>
 #include <vector>
 #include <algorithm>
 using namespace std;
 
-bool map[101][101];
-vector<pair<int, int>> vt;
+#define MAXS 101
+#define MAXN 10001
 
+typedef struct optimal_return
+{
+    int row, col, cnt;
+} opt_ret;
+
+int map[MAXS][MAXS], cnt, ans[MAXN][3];
 int costs[4] = {0, 2, 4, 7};
+int s, n;
+
+bool chk(int row, int col) { return 0<row&&row<=s&&0<col&&col<=s; }
+
+/**
+ * row, col : 시작 좌표
+ * size : 탐색하고자 하는 정사각형 너비
+ * return : 정사각형 안에 벗겨진곳 갯수
+ */
+int calc_damaged(int row, int col, int size)
+{
+    int cnt=0;
+    for (int i=0; i<size; i++)
+        for (int j=0; j<size; j++)
+            if (map[i+row][j+col] == 1) cnt++;
+
+    return cnt;
+}
+
+/**
+ * row, col : 시작 좌표
+ * size : 탐색하고자 하는 정사각형 너비
+ * panel_size  : 도금판 너비
+ * return : panel_size 인 도금판을 이용하여 덮었을때 가장 많이 복구되는 곳?
+ */
+opt_ret optimize(int row, int col, int size, int panel_size)
+{
+    int max_r = -1, max_c = -1, max_cnt = 0;
+    for (int i=0; i<size-panel_size+1; i++)
+        for (int j=0; j<size-panel_size+1; j++)
+        {
+            if (!chk(i+row+panel_size-1, j+col+panel_size-1)) continue;
+            int cnt = calc_damaged(i+row, j+col, panel_size);
+            if (max_cnt < cnt)
+            {
+                max_cnt = cnt;
+                max_r = i+row;
+                max_c = j+col;
+            }
+        }
+    
+    return { max_r, max_c, max_cnt };
+}
+
+void set_values(int row, int col, int panel_size, int before, int after)
+{
+    for (int i=0; i<panel_size; i++)
+        for (int j=0; j<panel_size; j++)
+            if (map[i+row][j+col] == before) map[i+row][j+col] = after;
+}
+
+int solve(int row, int col, int panel_size, bool isFirst)
+{
+    int sum_cost=0;
+    while (panel_size > 1)
+    {
+        // 전체 map 에서 panel_size 를 이용하였을때 가장 많이 복구되는 곳을 찾는다.
+        opt_ret optimals1 = optimize(row, col, s, panel_size);
+        if (optimals1.cnt == 0) break; // 없으면 끝
+        int cost1 = costs[panel_size]; // 가격.
+
+        // 처음 실행되는 것이고, panel_size 가 3이면?
+        if (isFirst && panel_size == 3)
+        {
+            int cost_1 = solve(row, col, 3, false); // optimals1 을 3으로 하고..
+            int cost_2 = solve(row, col, 2, false); // optimals1 을 2로 하고..
+            if (cost_2 < cost_1)
+            {
+                panel_size = 2;
+                continue;
+            }
+        }
+
+        int cost2 = 0;
+        if (panel_size == 3)
+        {
+            // 2 로 한번 해보자.
+            opt_ret optimals2 = optimize(optimals1.row, optimals1.col, 3, 2);
+            cost2 = costs[2];
+            set_values(optimals2.row, optimals2.col, 2, 1, 2);
+            
+        }
+    }
+}
 
 int main(int argc, char const *argv[])
 {
     int T; scanf("%d", &T);
-    for (int tc=1,s, n; tc<=T; tc++)
+    for (int tc=1; tc<=T; tc++)
     {
         scanf("%d%d", &s, &n);
-        for (int i=0; i<s; i++) for (int j=0; j<s; j++) map[i][j] = false;
+        memset(map, 0, sizeof(map));
         for (int i=0, r, c; i<n; i++)
         {
             scanf("%d%d", &r, &c);
-            map[r][c] = true;
-            vt.push_back({r+c, c});
+            map[r][c] = 1;
         }
 
-        sort(vt.begin(), vt.end());
-
-        for (auto x : vt)
-        {
-            int row = x.first - x.second;
-            int col = x.second;
-            printf("%d %d\n", row, col);
-        }
+        cnt = 0;
+        solve(0, 0, 3, true);
     }
     return 0;
 }
