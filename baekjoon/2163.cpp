c83f535a947661174ac54901805db0a64ee4dbdb
Dongonee <dongwon5520@gmail.com>
2019-08-27 23:25:52 +0900

    BOJ#2163 MATH

---
+++ baekjoon/2163.cpp
@@ -0,0 +1,13 @@
+/**
+ * 2019-08-27 22:53 ~ 
+ */
+#include <cstdio>
+
+int n, m;
+
+int main(int argc, char const *argv[])
+{
+    scanf("%d%d", &n, &m);
+    printf("%d", n*m-1);
+    return 0;
+}
