032843d738b5a54ee8c34cded79c90e8c2bb7b44
DongOnee <dongwon5520@gmail.com>
2019-12-10 16:13:25 +0900

    BOJ#3053 Math EZ

---
+++ baekjoon/3053.cpp
@@ -0,0 +1,10 @@
+#include <cstdio>
+#include <cmath>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    int r; scanf("%d", &r);
+    printf("%.6f\n%.6f\n", M_PI*r*r, 2.0*r*r);
+    return 0;
+}
